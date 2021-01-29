f22f1cb09a18b62e9c583d8742720a2dccf428df
Andrew Boyd <andrewboyd@Andrews-Air.attlocal.net>
2021-01-29 13:59:41 -0600

    Abstracted processing function

--- calculator.java
+++ calculator.java
@@ -1,28 +1,39 @@
 public class calculator {
+	
 	public static void main(String[] args) {
+		System.out.println("Welcome to calculator! Enter an expression to evaluate it or enker \"k\" when finished");
+		String input = "";
+		while(!input.equals("k")) {
+			input = System.console().readLine();
+			if(input.equals("k")) {
+				continue;
+			}
+			System.out.println(processExpression(input));
+		}
+	}
+	
+	public static double processExpression(String expression) {
 		int a = 0;
 		int b = 0;
 		int i;
-		for(i = 0; 0 <= args[0].charAt(i)-'0' && args[0].charAt(i)-'0' <=9 ; i++) {
-			a = a*10+args[0].charAt(i)-'0';
+		for(i = 0; 0 <= expression.charAt(i)-'0' && expression.charAt(i)-'0' <=9 ; i++) {
+			a = a*10+expression.charAt(i)-'0';
 		}
-		int c = args[0].charAt(i);
-		for(i++; i < args[0].length(); i++) {
-			b = b*10 + args[0].charAt(i)-'0';
+		int c = expression.charAt(i);
+		for(i++; i < expression.length(); i++) {
+			b = b*10 + expression.charAt(i)-'0';
 		}
 		switch (c){
 		case '+':
-			System.out.println(a+b);
-			break;
+			return (a+b);
 		case '-':
-			System.out.println(a-b);
-			break;
+			return (a-b);
 		case '*':
-			System.out.println(a*b);
-			break;
+			return (a*b);
 		case '/':
-			System.out.println(a/b);
-			break;
+			return (a/b);
 		}
+		return 0;
 	}
+	
 };
